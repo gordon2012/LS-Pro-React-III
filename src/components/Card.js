72ab5808f81233d3de6de9103404e29a67bbb7cf
Gordon Doskas <gordon2012@users.noreply.github.com>
2017-04-23 21:05:47 -0700

    Add overlay to Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,75 @@
 import React, { Component } from 'react';
-import '../App.css';
+import FontAwesome from 'react-fontawesome';
 
 export default class Card extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hover: false
+        };
+
+        this._handleHover = this._handleHover.bind(this);
+    }
+
+    _handleHover() {
+        console.log('hover');
+        this.setState({
+            hover: !this.state.hover
+        });
+    }
+
     render() {
-        const {url} = this.props.data;
-        return (    
-            <div className="Card">
-                <div className="Card-inner" style={{backgroundImage: `url(${url})`}}></div>
+        const {url, hearts, comments} = this.props.data;
+
+        const style = {
+            wrap: {
+                flexBasis: '33.33%',
+                height: '0',
+                paddingBottom: '33.33%',
+                position: 'relative',
+            },
+            inner: {
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                backgroundImage: `url(${url})`,
+                position: 'absolute',
+                top: '8px',
+                left: '8px',
+                right: '8px',
+                bottom: '8px'
+            },
+            span: {
+                zIndex: 11,
+                color: 'white'
+            },
+            overlay: {
+                position: 'absolute',
+                top: '8px',
+                right: '8px',
+                left: '8px',
+                bottom: '8px',
+                backgroundColor: 'rgba(0,0,0,0.5)',
+            },
+            caption: {
+                border: '0px solid red',
+                color: 'white',
+                padding: '40% 0',
+            },
+            hide: {
+                display: 'none'
+            },
+        };
+
+        return (
+            <div style={style.wrap} onMouseEnter={this._handleHover} onMouseLeave={this._handleHover}>
+                <div style={style.inner}></div>
+                <div style={this.state.hover ? style.overlay : style.hide }>
+                    <div style={style.caption} className="columns">
+                        <div className="column is-6 has-text-centered"><FontAwesome name="heart"/> &nbsp; {hearts}</div>
+                        <div className="column is-6 has-text-centered"><FontAwesome name="comment"/> &nbsp; {comments}</div>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import 'font-awesome/css/font-awesome.min.css'
+
+
 
 const initialProps = {
     user: {
@@ -10,15 +13,46 @@ const initialProps = {
       bio: 'Italian greyhound, insatiable adventurer, avid wrestler, faithful familiar.'
     },
     posts: [
-        { title: 'Nap Time', url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/303256/20170315_074622.jpg' },
-        { title: 'My Favorite Sky', url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/303256/20170131_165348.jpg' },
-        { title: 'You Want My Toy', url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/303256/20161206_163059.jpg' },
-        { title: 'Wrestle!', url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/303256/20161130_220314.jpg' },
-        { title: 'Just Chillin\'', url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/303256/20161005_145148.jpg' },
+        {
+          title: 'Nap Time',
+          url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/303256/20170315_074622.jpg',
+          hearts: 5,
+          comments: 3
+        },
+        {
+          title: 'My Favorite Sky',
+          url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/303256/20170131_165348.jpg',
+          hearts: 2,
+          comments: 3
+        },
+        {
+          title: 'You Want My Toy',
+          url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/303256/20161206_163059.jpg',
+          hearts: 5,
+          comments: 1
+        },
+        {
+          title: 'Wrestle!',
+          url: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/303256/20161130_220314.jpg',
+          hearts: 2,
+          comments: 13
+        },
+        {
+          title: 'Just Chillin\'',
+          url: 'http://lorempixel.com/400/500',
+          hearts: 17,
+          comments: 6
+        },
+        {
+          title: 'Good Stuff',
+          url: 'http://lorempixel.com/500/600',
+          hearts: 27,
+          comments: 16
+        },
+
     ]
 };
 
 ReactDOM.render(
-  React.createElement(App, initialProps, null),
-  document.getElementById('root')
+  React.createElement(App, initialProps, null), document.getElementById('root')
 );
